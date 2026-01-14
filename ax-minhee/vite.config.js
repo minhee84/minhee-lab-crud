4f43599f519848bce4711752a33240c508b786e2
minhee <seohan35@gmail.com>
2026-01-15 00:20:58 +0900

    2026-01-14-2

--- ax-minhee/vite.config.js
+++ ax-minhee/vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Vercel 배포 시에는 보통 '/'를 사용합니다.
 })
